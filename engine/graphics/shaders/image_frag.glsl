4df6de981aa95480b7f5a775c4e82e5be9d98249
teoma <teoma@Teo>
2026-04-29 16:38:14 +0300

    changes on shader

--- engine/graphics/shaders/image_frag.glsl
+++ engine/graphics/shaders/image_frag.glsl
@@ -8,8 +8,8 @@ uniform sampler2D maskimage;
 
 uniform vec4 spriteColor;
 uniform int maskMode;
-uniform float imageRotation;
 uniform float maskRotation;
+uniform float imageRotation;
 
 const int MASKMODE_BOTH = 0;
 const int MASKMODE_PICTURE = 1;
@@ -20,41 +20,40 @@ void main() {
     vec2 pivot = vec2(0.5, 0.5);
     vec2 uv = TexCoords - pivot;
 
+    float maskAngle = radians(maskRotation);
+    float maskC = cos(maskAngle);
+    float maskS = sin(maskAngle);
     float imageAngle = radians(imageRotation);
     float imageC = cos(imageAngle);
     float imageS = sin(imageAngle);
 
-    float maskAngle = radians(maskRotation);
-    float maskC = cos(maskAngle);
-    float maskS = sin(maskAngle);
-
-    vec2 imageRotated = vec2(
-        uv.x * imageC + uv.y * imageS,
-       -uv.x * imageS + uv.y * imageC
-    ) + pivot;
-
     vec2 maskRotated = vec2(
-        uv.x * maskC + uv.y * maskS,
-       -uv.x * maskS + uv.y * maskC
+    uv.x * maskC + uv.y * maskS,
+   -uv.x * maskS + uv.y * maskC
+    ) + pivot;
+    
+    vec2 imageRotated = vec2(
+    uv.x * imageC + uv.y * imageS,
+   -uv.x * imageS + uv.y * imageC
     ) + pivot;
 
     vec4 tex;
     vec4 msk;
 
     if(maskMode == MASKMODE_BOTH) {
-        tex = texture(image, imageRotated);
+        tex = texture(image, TexCoords);
         msk = texture(maskimage, TexCoords);
     }
     else if(maskMode == MASKMODE_PICTURE) {
         tex = texture(image, imageRotated);
-        msk = texture(maskimage, imageRotated);
+        msk = texture(maskimage, TexCoords);
     }
     else if(maskMode == MASKMODE_MASK) {
         tex = texture(image, TexCoords);
         msk = texture(maskimage, maskRotated);
     }
     else if(maskMode == MASKMODE_BOTH_FREE_ROTATE) {
-        tex = texture(image, imageRotated);
+        tex = texture(image, TexCoords);
         msk = texture(maskimage, maskRotated);
     }
     else {
