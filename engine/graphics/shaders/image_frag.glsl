5e8d3a209a35631c8c7844a8ea98664d636a55e2
teoma <teoma@Teo>
2026-05-06 14:38:46 +0300

    changes on textures

--- engine/graphics/shaders/image_frag.glsl
+++ engine/graphics/shaders/image_frag.glsl
@@ -9,19 +9,62 @@ out vec4 color;
 
 uniform sampler2D image;
 uniform sampler2D maskimage;
-uniform vec4 spriteColor;
-uniform int isAlphaMasking;
 
-vec4 mask;
+uniform vec4 spriteColor;
+uniform int maskMode;
+uniform float maskRotation;
+uniform float imageRotation;
+
+const int MASKMODE_BOTH = 0;
+const int MASKMODE_PICTURE = 1;
+const int MASKMODE_MASK = 2;
+const int MASKMODE_BOTH_FREE_ROTATE = 3;
 
 void main() {
-	color = spriteColor * texture(image, TexCoords);
-	if(isAlphaMasking == 1) {
-       mask = texture(maskimage, TexCoords);
-       mask.a = 1.0 - mask.r;
-       mask.r = 1.0;
-       mask.g = 1.0;
-       mask.b = 1.0;
-       color *= mask;
+    vec2 pivot = vec2(0.5, 0.5);
+    vec2 uv = TexCoords - pivot;
+
+    float maskAngle = radians(maskRotation);
+    float maskC = cos(maskAngle);
+    float maskS = sin(maskAngle);
+    float imageAngle = radians(imageRotation);
+    float imageC = cos(imageAngle);
+    float imageS = sin(imageAngle);
+
+    vec2 maskRotated = vec2(
+    uv.x * maskC + uv.y * maskS,
+   -uv.x * maskS + uv.y * maskC
+    ) + pivot;
+    
+    vec2 imageRotated = vec2(
+    uv.x * imageC + uv.y * imageS,
+   -uv.x * imageS + uv.y * imageC
+    ) + pivot;
+
+    vec4 tex;
+    vec4 msk;
+
+    if(maskMode == MASKMODE_BOTH) {
+        tex = texture(image, TexCoords);
+        msk = texture(maskimage, TexCoords);
     }
+    else if(maskMode == MASKMODE_PICTURE) {
+        tex = texture(image, imageRotated);
+        msk = texture(maskimage, TexCoords);
+    }
+    else if(maskMode == MASKMODE_MASK) {
+        tex = texture(image, TexCoords);
+        msk = texture(maskimage, maskRotated);
+    }
+    else if(maskMode == MASKMODE_BOTH_FREE_ROTATE) {
+        tex = texture(image, TexCoords);
+        msk = texture(maskimage, maskRotated);
+    }
+    else {
+        tex = texture(image, TexCoords);
+        msk = texture(maskimage, TexCoords);
+    }
+
+    float maskAlpha = 1.0 - msk.r;
+    color = spriteColor * vec4(tex.rgb, tex.a * maskAlpha);
 }
