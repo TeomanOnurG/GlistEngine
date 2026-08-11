fa59de60584184dcca9bb1287273a4d43a0bfcff
teoma <teoma@Teo>
2026-08-11 18:29:35 +0300

    Add 7 mods alpha masking

---
+++ samples/gisAlphaMasking/src/GameCanvas.h
@@ -0,0 +1,64 @@
+/*
+ * GameCanvas.h
+ *
+ *  Created on: May 6, 2020
+ *      Author: noyan
+ */
+
+#ifndef GAMECANVAS_H_
+#define GAMECANVAS_H_
+
+#include "gBaseCanvas.h"
+#include "gApp.h"
+#include "gImage.h"
+#include "gFont.h"
+
+class GameCanvas : public gBaseCanvas {
+public:
+	GameCanvas(gApp* root);
+	virtual ~GameCanvas();
+
+	void setup();
+	void update();
+	void draw();
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void charPressed(unsigned int codepoint);
+
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseScrolled(int x, int y);
+
+	void mouseEntered();
+	void mouseExited();
+
+	void windowResized(int w, int h);
+
+	void showNotify();
+	void hideNotify();
+
+private:
+	gApp* root;
+
+	gImage imageBoth;
+	gImage imagePicture;
+	gImage imageMask;
+	gImage imageFree;
+	gImage image;
+	gImage mask;
+	gImage imageMaskNoRotation;
+	gImage engineLogo;
+
+	gFont font;
+
+	float bothAngle;
+	float pictureAngle;
+	float maskAngle;
+	float freeImageAngle;
+	float freeMaskAngle;
+};
+
+#endif /* GAMECANVAS_H_ */
